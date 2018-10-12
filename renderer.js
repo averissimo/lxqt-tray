c34ef1189b905b041e78707094cf54dd7dbfae26
André Veríssimo <afsverissimo@gmail.com>
2018-10-12 19:15:11 +0100

    does not work in lxqt

--- renderer.js
+++ renderer.js
@@ -1,3 +1,21 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const { remote } = require('electron')
+const { Menu, Tray, BrowserWindow } = remote
+const path = require('path')
+
+const tray = new Tray(path.join(__dirname,'icon-read_128.png'))
+tray.setToolTip('caca')
+
+const myMenu = [
+  {
+    label: 'Switch tray icon',
+    click: () => {
+      tray.setImage(path.join(__dirname,'icon-unread_128.png'))
+    }
+  }
+]
+
+Menu.setApplicationMenu(Menu.buildFromTemplate(myMenu))
